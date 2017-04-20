4326da9a3493757377e19b6614513fa609167ecf
Nick Sifniotis <nick_sifniotis@hotmail.com>
2017-04-20 21:03:34 +1000

    Added hunter1st email, sent email out o participants

--- main.py
+++ main.py
@@ -10,10 +10,18 @@ database = database.Database()
 emails = Email.LoadAll(database)
 people = participant.LoadAll(database)
 
-email = emails[0]
-timestamp = int(time.time())
-batch_id = database.ExecuteInsert("INSERT INTO batch (email_id, datetime) VALUES (" +
-                                  str(email.ID()) + ", " + str(timestamp) + ");")
-BatchManager.SendEmailToGroup(4, batch_id, email, people, database)
+# hard coded the value of the reminder email, because I suck.
+email = None
+for candidate in emails:
+    if candidate.ID() is 3:
+        email = candidate
+
+if email:
+    timestamp = int(time.time())
+    batch_id = database.ExecuteInsert("INSERT INTO batch (email_id, datetime) VALUES (" +
+                                      str(email.ID()) + ", " + str(timestamp) + ");")
+    
+    BatchManager.SendEmailToGroup(4, batch_id, email, people, database)
+    BatchManager.SendEmailToGroup(3, batch_id, email, people, database)
 
 database.Close()
